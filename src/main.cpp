f09abbe88c457bc1cc5a08f3fcaf1b340ded48c5
axthur <arthurfabriresende@gmail.com>
2025-06-25 17:40:00 -0300

    Now is Pac-Mario

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,327 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+#define COMPRIMENTO 400
+#define LARGURA 420
+#define PIXEL 16
+
+const int mapSize = 25;
+char map[mapSize][mapSize + 1] = {
+    "1111111111111111111111111",
+    "1000000000000000000000001",
+    "1011110111110111110111101",
+    "1010000100000100000100001",
+    "1010110111110111110101101",
+    "1000100001000001000000101",
+    "1111111011111110111111111",
+    "1000001000000000001000001",
+    "1011111011110111111011101",
+    "1000000010000100000000001",
+    "1011111110111111011111101",
+    "1000000000100001000000001",
+    "1011111111110111111111101",
+    "1000000000000000000000001",
+    "1111111110111111011111111",
+    "1000000010000100000000001",
+    "1011111011110111111011101",
+    "1000001000000000001000001",
+    "1111111011111110111111111",
+    "1000100001000001000000101",
+    "1010110111110111110101101",
+    "1010000100000100000100001",
+    "1011110111110111110111101",
+    "1000000000000000000000001",
+    "1111111111111111111111111"};
+
+enum Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+bool up = false, down = false, left = false, right = true;
+
+struct Entity
+{
+    int x, y;
+    sf::Texture texture, rightTexture, leftTexture, upTexture, downTexture;
+    sf::Sprite sprite;
+    Direction currentDirection, intentionDirection;
+};
+
+int coins = 0;
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    // Renderizando a janela:
+    sf::RenderWindow window(sf::VideoMode(COMPRIMENTO, LARGURA), "Pac-Mario");
+    sf::Image icon;
+    icon.loadFromFile("./assets/icon.png");
+    window.setIcon(16, 16, icon.getPixelsPtr());
 
+    // Carregando o fundo:
+    sf::Texture backgroundTexture;
+    backgroundTexture.loadFromFile("./assets/map.png");
+    sf::Sprite background(backgroundTexture);
+
+    // Carregando as paredes:
+    sf::Texture blockTexture;
+    blockTexture.loadFromFile("./assets/block.png");
+    sf::Sprite blockSprite(blockTexture);
+
+    // Carregando as moedas:
+    sf::Texture coinTexture;
+    coinTexture.loadFromFile("./assets/coin.png");
+    sf::Sprite coinSprite(coinTexture);
+
+    // Carregando o contador de moedas:
+    sf::Font font;
+    if (!font.loadFromFile("./assets/super_mario_world.ttf"))
+    {
+        std::cerr << "Erro ao carregar a fonte!" << std::endl;
+        return 1;
+    }
+
+    sf::Text coinText;
+    coinText.setFont(font);
+    coinText.setCharacterSize(8);
+    coinText.setFillColor(sf::Color::White);
+    coinText.setPosition(22, 404);
+
+    // Contando o total de moedas:
+    int totalCoins = 0;
+    for (int i = 0; i < mapSize; i++)
+        for (int j = 0; j < mapSize; j++)
+            if (map[i][j] == '0')
+                totalCoins++;
+
+    sf::Texture counterTexture;
+    counterTexture.loadFromFile("./assets/counter.png");
+    sf::Sprite counterSprite(counterTexture);
+    counterSprite.setPosition(5, 405);
+
+    coinText.setString("0 de " + std::to_string(totalCoins));
+
+    // Carregando o Mario:
+    Entity player;
+    player.x = 12;
+    player.y = 11;
+
+    player.rightTexture.loadFromFile("./assets/mario_right.png");
+    player.leftTexture.loadFromFile("./assets/mario_left.png");
+    player.upTexture.loadFromFile("./assets/mario_up.png");
+    player.downTexture.loadFromFile("./assets/mario_down.png");
+
+    player.sprite.setTexture(player.downTexture);
+
+    // player.currentDirection = player.intentionDirection = RIGHT;
+
+    // Carregando as tartarugas:
+    Entity turtles[6];
+
+    // Tartarugas nas pontas
+    turtles[0].x = turtles[0].y = turtles[1].x = turtles[2].y = 1;
+    turtles[1].y = turtles[2].x = turtles[3].x = turtles[3].y = 23;
+
+    // Novas tartarugas no centro esquerdo e direito
+    turtles[4].x = 1;
+    turtles[4].y = 12;
+    turtles[5].x = 23;
+    turtles[5].y = 12;
+
+    // Direções iniciais
+    turtles[0].currentDirection = turtles[0].intentionDirection = DOWN;
+    turtles[1].currentDirection = turtles[1].intentionDirection = RIGHT;
+    turtles[2].currentDirection = turtles[2].intentionDirection = UP;
+    turtles[3].currentDirection = turtles[3].intentionDirection = LEFT;
+    turtles[4].currentDirection = turtles[4].intentionDirection = RIGHT;
+    turtles[5].currentDirection = turtles[5].intentionDirection = LEFT;
+
+    for (int i = 0; i < 6; i++)
+    {
+        turtles[i].texture.loadFromFile("./assets/shell.gif");
+        turtles[i].sprite.setTexture(turtles[i].texture);
+    }
+
+    // Definindo o clock:
+    sf::Clock clock;
+
+    // Enquanto a janela estiver aberta:
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            if (event->is<sf::Event::Closed>())
-            {
+            if (event.type == sf::Event::Closed)
                 window.close();
+
+            // Se pressiona uma tecla:
+            if (event.type == sf::Event::KeyPressed)
+            {
+                // Intenção de direção = direção atual
+                player.intentionDirection = player.currentDirection;
+
+                // Define a intenção de direção:
+                switch (event.key.code)
+                {
+                case sf::Keyboard::Left:
+                    player.intentionDirection = LEFT;
+                    break;
+                case sf::Keyboard::Right:
+                    player.intentionDirection = RIGHT;
+                    break;
+                case sf::Keyboard::Up:
+                    player.intentionDirection = UP;
+                    break;
+                case sf::Keyboard::Down:
+                    player.intentionDirection = DOWN;
+                    break;
+                default:
+                    break;
+                }
             }
         }
 
-        window.clear();
+        // Atualiza a cada 0.2 segundos:
+        char coord;
+        if (clock.getElapsedTime() > sf::seconds(0.2f))
+        {
+            clock.restart();
+            int newX, newY;
+
+            // Carrega a nova posição das tartarugas:
+            for (int i = 0; i < 6; i++)
+            {
+                newX = turtles[i].x, newY = turtles[i].y;
+                if (turtles[i].intentionDirection == LEFT)
+                    newX--;
+                else if (turtles[i].intentionDirection == RIGHT)
+                    newX++;
+                else if (turtles[i].intentionDirection == UP)
+                    newY--;
+                else if (turtles[i].intentionDirection == DOWN)
+                    newY++;
+
+                coord = map[newY][newX];
+                // Se for uma parede:
+                if (coord == '1')
+                {
+                    Direction newDirection = Direction(rand() % 4);
+                    while (turtles[i].intentionDirection == newDirection)
+                        newDirection = Direction(rand() % 4);
+
+                    turtles[i].intentionDirection = newDirection;
+                }
+                // Se não for uma parede:
+                else
+                {
+                    turtles[i].currentDirection = turtles[i].intentionDirection;
+
+                    turtles[i].x = newX;
+                    turtles[i].y = newY;
+                }
+            }
+
+            // Carrega a nova posição do Mario:
+            newX = player.x, newY = player.y;
+
+            if (player.intentionDirection == LEFT)
+                newX--;
+            else if (player.intentionDirection == RIGHT)
+                newX++;
+            else if (player.intentionDirection == UP)
+                newY--;
+            else if (player.intentionDirection == DOWN)
+                newY++;
+
+            coord = map[newY][newX];
+            // Se não for uma parede:
+            if (coord != '1')
+            {
+                if (player.intentionDirection == LEFT)
+                {
+                    left = true;
+                    right = up = down = false;
+                    player.sprite.setTexture(player.leftTexture);
+                }
+                else if (player.intentionDirection == RIGHT)
+                {
+                    right = true;
+                    left = up = down = false;
+                    player.sprite.setTexture(player.rightTexture);
+                }
+                else if (player.intentionDirection == UP)
+                {
+                    up = true;
+                    down = left = right = false;
+                    player.sprite.setTexture(player.upTexture);
+                }
+                else if (player.intentionDirection == DOWN)
+                {
+                    down = true;
+                    up = left = right = false;
+                    player.sprite.setTexture(player.downTexture);
+                }
+
+                player.currentDirection = player.intentionDirection;
+
+                player.x = newX;
+                player.y = newY;
+
+                std::cout << player.x << ", " << player.y << std::endl;
+                if (coord == '0')
+                {
+                    map[newY][newX] = ' ';
+                    coins++;
+                    coinText.setString(std::to_string(coins) + " de " + std::to_string(totalCoins));
+                }
+            }
+        }
+
+        // Desenha o fundo:
+        window.clear(sf::Color::Black);
+        window.draw(background);
+
+        // Desenha o mapa:
+        for (int i = 0; i < mapSize; i++)
+        {
+            for (int j = 0; j < mapSize; j++)
+            {
+                coord = map[i][j];
+                if (coord == '1')
+                {
+                    blockSprite.setPosition(j * PIXEL, i * PIXEL);
+                    window.draw(blockSprite);
+                }
+                else if (coord == '0')
+                {
+                    coinSprite.setPosition(j * PIXEL, i * PIXEL);
+                    window.draw(coinSprite);
+                }
+            }
+        }
+
+        // Desenha as tartarugas:
+        for (int i = 0; i < 6; i++)
+        {
+            turtles[i].sprite.setPosition(turtles[i].x * PIXEL, turtles[i].y * PIXEL);
+            window.draw(turtles[i].sprite);
+        }
+
+        // Desenha o Mario:
+        player.sprite.setPosition(player.x * PIXEL, player.y * PIXEL);
+        window.draw(player.sprite);
+
+        // Desenha o contador de moedas:
+        window.draw(counterSprite);
+        window.draw(coinText);
+
+        // Exibe a janela:
         window.display();
     }
+
+    return 0;
 }
